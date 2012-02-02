481e1f8281aac7b1a95f59bf9124361ed5172d80
unknown <Anna@.(none)>
2012-02-01 21:46:15 -0700

    Edit #2 Anna

--- Scanner.py
+++ Scanner.py
@@ -2,17 +2,27 @@ import re
 
 class Scanner():
 
-    def __init__(self):
-        self.contents = []
+    def __init__(self, in_file):
+        self.file = in_file
+        self.symbols = ['.', ',', '(', ')', '=', '+', '-', '*']
+        self.hash =
 
     def open_file(self, input_file):
-        f = open(input_file)
+        self.file = open(input_file)
 
     def get_token(self):
-        for line in self.contents:
-            print line
-            re.sub(r'\s', '', line)
-            print line
+        cur = self.file.read(1)
+        val = 0
+        for pattern in t_dict:
+            result = re.match(pattern, cur)
+            if result.group(0):
+                val = t_dict.index(pattern)
+
+
+        
+
+        re.sub(r'\s', '', line)
+        print line
 
     def get_lexeme(self):
         pass
