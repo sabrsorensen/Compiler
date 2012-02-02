facfa4f75d9043162c0856070f9eef6023f43f9b
Sam Sorensen <sabrsorensen@gmail.com>
2012-02-01 22:08:04 -0700

    Keywords!

--- Scanner.py
+++ Scanner.py
@@ -2,6 +2,13 @@ import re
 
 class Scanner():
 
+    keywords = {'and':'MP_AND','begin':'MP_BEGIN','div':'MP_DIV','do':'MP_DO',
+                'downto':'MP_DOWNTO','else':'MP_ELSE','end':'MP_END','fixed':'MP_FIXED',
+                'float':'MP_FLOAT','for':'MP_FOR','function':'MP_FUNCTION','if':'MP_IF',
+                'integer':'MP_INTEGER','mod':'MP_MOD','not':'MP_NOT','or':'MP_OR',
+                'procedure':'MP_PROCEDURE','program':'MP_PROGRAM','read':'MP_READ',
+                'repeat':'MP_REPEAT','then':'MP_THEN','to':'MP_TO','until':'MP_UNTIL',
+                'var':'MP_VAR','while':'MP_WHILE','write':'MP_WRITE'}
     def __init__(self, in_file):
         self.file = in_file
         self.symbols = ['.', ',', '(', ')', '=', '+', '-', '*']
@@ -24,52 +31,12 @@ class Scanner():
         re.sub(r'\s', '', line)
         print line
 
-
     def get_lexeme(self):
         pass
     def get_line(self):
         pass
     def get_column(self):
         pass
-
-    #distributor sub-methods
-    def t_period(self):
-        pass
-    def t_comma(self):
-        pass
-    def t_semicolon(self):
-        pass
-    def t_l_paren(self):
-        pass
-    def t_r_paren(self):
-        pass
-    def t_eq(self):
-        pass
-    def t_gt(self):
-        pass
-    def t_lt(self):
-        pass
-    def t_colon(self):
-        pass
-    def t_plus(self):
-        pass
-    def t_minus(self):
-        pass
-    def t_mul(self):
-        pass
-    def t_id_key(self):
-        pass
-    def t_num(self):
-        pass
-    def t_string(self):
-        pass
-    def t_l_comment(self):
-        pass
-    def t_r_comment(self):
-        pass
-
-
-
 
 s = Scanner()
 s.open_file('C:\Users\Anna\Documents\Code\Aptana\workspace\Compiler\sample.txt')
