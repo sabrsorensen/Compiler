34afecbcfc0a5c8a5d977588856d4bda149b634d
Sam Sorensen <sabrsorensen@gmail.com>
2012-02-02 18:36:43 -0700

    Create_token added

--- Scanner.py
+++ Scanner.py
@@ -1,20 +1,28 @@
 import re
+import os
+import sys
+
 
 class Scanner():
 
-    keywords = {'and':'MP_AND','begin':'MP_BEGIN','div':'MP_DIV','do':'MP_DO',
-                'downto':'MP_DOWNTO','else':'MP_ELSE','end':'MP_END','fixed':'MP_FIXED',
-                'float':'MP_FLOAT','for':'MP_FOR','function':'MP_FUNCTION','if':'MP_IF',
-                'integer':'MP_INTEGER','mod':'MP_MOD','not':'MP_NOT','or':'MP_OR',
-                'procedure':'MP_PROCEDURE','program':'MP_PROGRAM','read':'MP_READ',
-                'repeat':'MP_REPEAT','then':'MP_THEN','to':'MP_TO','until':'MP_UNTIL',
-                'var':'MP_VAR','while':'MP_WHILE','write':'MP_WRITE'}
-    def __init__(self, in_file):
+
+    def __init__(self, in_file=None):
         self.file = in_file
         self.symbols = ['\.',',','(',')','=','>','<',':','+','-','*','\w','\d','\'','{','}']
+        self.keywords = {'and':'MP_AND','begin':'MP_BEGIN','div':'MP_DIV','do':'MP_DO',
+                         'downto':'MP_DOWNTO','else':'MP_ELSE','end':'MP_END','fixed':'MP_FIXED',
+                         'float':'MP_FLOAT','for':'MP_FOR','function':'MP_FUNCTION','if':'MP_IF',
+                         'integer':'MP_INTEGER','mod':'MP_MOD','not':'MP_NOT','or':'MP_OR',
+                         'procedure':'MP_PROCEDURE','program':'MP_PROGRAM','read':'MP_READ',
+                         'repeat':'MP_REPEAT','then':'MP_THEN','to':'MP_TO','until':'MP_UNTIL',
+                         'var':'MP_VAR','while':'MP_WHILE','write':'MP_WRITE'}
 
     def open_file(self, input_file):
         self.file = open(input_file)
+
+    def create_token(self, tok):
+        s = Token(self.keywords[tok],get_line(),get_column(), tok)
+        tokens.append(s)
 
     def get_token(self):
         cur = self.file.read(1)
@@ -103,5 +111,5 @@ class Token(object):
         return "%16s %6s %4s %s" % (self.token_type, self.line, self.column, self.token_value)
 
 s = Scanner()
-s.open_file('C:\Users\Anna\Documents\Code\Aptana\workspace\Compiler\sample.txt')
+s.open_file(r'.\sample.txt')
 s.get_token()
