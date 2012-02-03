808dce80562ac14b001c12c4e285c54630778b64
Sam Sorensen <sabrsorensen@gmail.com>
2012-02-02 19:01:54 -0700

    Changed create_token to match Keith's methods.

--- Scanner.py
+++ Scanner.py
@@ -1,21 +1,30 @@
 import re
 from user import f
 
+
 class Scanner():
 
-    keywords = {'and':'MP_AND','begin':'MP_BEGIN','div':'MP_DIV','do':'MP_DO',
-                'downto':'MP_DOWNTO','else':'MP_ELSE','end':'MP_END','fixed':'MP_FIXED',
-                'float':'MP_FLOAT','for':'MP_FOR','function':'MP_FUNCTION','if':'MP_IF',
-                'integer':'MP_INTEGER','mod':'MP_MOD','not':'MP_NOT','or':'MP_OR',
-                'procedure':'MP_PROCEDURE','program':'MP_PROGRAM','read':'MP_READ',
-                'repeat':'MP_REPEAT','then':'MP_THEN','to':'MP_TO','until':'MP_UNTIL',
-                'var':'MP_VAR','while':'MP_WHILE','write':'MP_WRITE'}
-    def __init__(self, in_file):
+
+    def __init__(self, in_file=None):
         self.file = in_file
+        self.column
+        self.line
         self.symbols = ['\.',',','(',')','=','>','<',':','+','-','*','\w','\d','\'','{','}']
         self.tokens = []
+        self.keywords = {'and':'MP_AND','begin':'MP_BEGIN','div':'MP_DIV','do':'MP_DO',
+                         'downto':'MP_DOWNTO','else':'MP_ELSE','end':'MP_END','fixed':'MP_FIXED',
+                         'float':'MP_FLOAT','for':'MP_FOR','function':'MP_FUNCTION','if':'MP_IF',
+                         'integer':'MP_INTEGER','mod':'MP_MOD','not':'MP_NOT','or':'MP_OR',
+                         'procedure':'MP_PROCEDURE','program':'MP_PROGRAM','read':'MP_READ',
+                         'repeat':'MP_REPEAT','then':'MP_THEN','to':'MP_TO','until':'MP_UNTIL',
+                         'var':'MP_VAR','while':'MP_WHILE','write':'MP_WRITE'}
+
     def open_file(self, input_file):
         self.file = open(input_file)
+
+    def create_token(self, token_type, token_line, token_column, token_name ):
+        s = Token(token_type,token_line,token_column, token_name)
+        tokens.append(s)
 
     def get_token(self):
         cur = self.file.read(1)
@@ -59,34 +68,34 @@ class Scanner():
     t_id_key() passes '+' back to the distributor, and file object now points at 'c'
     '''
     def t_period(self, in_char):
-        self.token_type = 'MP_PERIOD'
-        self.line = get_line()
-        self.column = get_column()
-        self.token = in_char
-        create_token(self.token_type, self.line, self.column, self.token)
+        token_type = 'MP_PERIOD'
+        line = get_line()
+        column = get_column()
+        token = in_char
+        create_token(token_type, line, column, token)
         cur_char = f.read(1)
         return cur_char
     def t_comma(self, in_char):
-        self.token_type = 'MP_COMMA'
-        self.line = get_line()
-        self.column = get_column()
-        self.token = in_char
+        token_type = 'MP_COMMA'
+        line = get_line()
+        column = get_column()
+        token = in_char
         create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
     def t_semicolon(self, in_char):
-        self.token_type = 'MP_SCOLON'
-        self.line = get_line()
-        self.column = get_column()
-        self.token = in_char
+        token_type = 'MP_SCOLON'
+        line = get_line()
+        column = get_column()
+        token = in_char
         create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
     def t_l_paren(self, in_char):
-        self.token_type = 'MP_LPAREN'
-        self.line = get_line()
-        self.column = get_column()
-        self.token = in_char
+        token_type = 'MP_LPAREN'
+        line = get_line()
+        column = get_column()
+        token = in_char
         create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
@@ -95,23 +104,23 @@ class Scanner():
         self.line = get_line()
         self.column = get_column()
         self.token = in_char
-        create_token(self.token_type, self.line, self.column, self.token)
+        self.create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
     def t_eq(self, in_char):
         self.token_type = 'MP_EQUAL'
-        self.line = get_line()
-        self.column = get_column()
+        self.line = self.get_line()
+        self.column = self.get_column()
         self.token = in_char
-        create_token(self.token_type, self.line, self.column, self.token)
+        self.create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
     def t_plus(self, in_char):
         self.token_type = 'MP_PLUS'
-        self.line = get_line()
-        self.column = get_column()
+        self.line = self.get_line()
+        self.column = self.get_column()
         self.token = in_char
-        create_token(self.token_type, self.line, self.column, self.token)
+        self.create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
     def t_minus(self, in_char):
@@ -119,7 +128,7 @@ class Scanner():
         self.line = get_line()
         self.column = get_column()
         self.token = in_char
-        create_token(self.token_type, self.line, self.column, self.token)
+        self.create_token(self.token_type, self.line, self.column, self.token)
         cur_char = f.read(1)
         return cur_char
     def t_mul(self, in_char):
@@ -148,6 +157,15 @@ class Scanner():
     def t_r_comment(self, in_char):
         pass
 
+    def scanner_read_char(self):
+        self.cur = self.file.read(1)
+        if cur is '\n':
+            self.line += 1
+            self.column = 0
+        if cur is not '\n':
+            self.column += 1
+
+
 class Token(object):
     def __init__(self, token_type, line, column, token_value):
         self.token_type = token_type
@@ -158,5 +176,6 @@ class Token(object):
         return "%16s %6s %4s %s" % (self.token_type, self.line, self.column, self.token_value)
 
 s = Scanner()
-s.open_file('C:\Users\Anna\Documents\Code\Aptana\workspace\Compiler\sample.txt')
+s.open_file(r'.\sample.txt')
 s.get_token()
+
