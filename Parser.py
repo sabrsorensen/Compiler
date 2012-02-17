ab67b55ab64e28f3c868f80b656603ac7776fad1
Sam Sorensen <sabrsorensen@gmail.com>
2012-02-16 18:13:31 -0700

    getting the file in place and the framework for teh regex

---
+++ Parser.py
@@ -0,0 +1,11 @@
+__author__ = 'logiasin'
+import sys
+import re
+
+class Parser:
+
+    def __init__(self):
+        #### Regular Expressions out the wazoo. Yes, that is typically where they come from ####
+        self.ProgramRE
+
+
