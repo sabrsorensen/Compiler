8b3f8e4991742977506b216435f771471607fcdb
Sam Sorensen <logiasin@Erasmus.localdomain>
2012-04-09 15:37:52 -0600

    Fixed/corrected/cleaned  semantic_entry and started symbol_table

--- semantic_entry.py
+++ semantic_entry.py
@@ -1,8 +1,6 @@
 __authors__ = 'Sam Sorensen', 'Keith Smith', 'Anna Andriyanova'
 __date__ = 'Spring 2012'
 
-# according to PEP, class names are written in CamelBack notation
-# My mistake. Should we rename the filenames as well?
 class SemanticEntry(object):
 
     def __init__(self):
@@ -13,7 +11,8 @@ class SemanticEntry(object):
 
 
     def put(self, record):
-        self.semantic_record_stack.append(record)
+        if cur_record:
+            self.semantic_record_stack.insert(0,cur_record)
         self.cur_record = record
         self.depth += 1
 
@@ -24,28 +23,5 @@ class SemanticEntry(object):
             self.cur_record = None
         self.depth -= 1
 
-    # We don't really need the following three functions, python instance variables are public by default.
-    # Can I pull them out then, along with to_string?
-#    def get_current_record(self):
-#        return self.current_record
-#
-#    def is_empty(self):
-#        return self.empty
-#
-#    def get_depth(self):
-#        return self.depth
-
-    # Sam, remember __repr__ ? :)
-    # Hmm, nifty function.
     def __repr__(self):
         return "%s %s" % (self.cur_record, self.semantic_record_stack)
-
-#    def to_string(self):
-#        self.output.append(self.current_record)
-#        if self.semantic_record_stack is not None:
-#            for sem_record in semantic_record_stack:
-#                self.output.append(sem_record)
-#                self.output.append(" ")
-#
-#        self.output.append('\n')
-#        return output
--- symbol_table.py
+++ symbol_table.py
@@ -6,9 +6,11 @@ import logging
 class SymbolTable(object):
 
     def __init__(self, name, depth):
-        self.name = name
-        self.depth = depth
-        self.sem_records = []
+        self.name = name #What are we using this for?
+        self.cur_depth = depth
+        self.sem_records = [] #Do we need this?
+        self.contextAttributesStack = []
+        self.sym_table = {}
 
 
 class ContextAttrs(object):
@@ -18,4 +20,3 @@ class ContextAttrs(object):
         self.nesting_level = 1
         self.cur_offset = None
         # note: no need for size; len(ex.context_lexemes) works.
-
