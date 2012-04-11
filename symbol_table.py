5b9ba1bd56607f09d93abf837eeae00bf86458b9
Sam Sorensen <sabrsorensen@gmail.com>
2012-04-10 20:46:40 -0600

    symbol_table should be complete now. Please look it over when you have a chance.

--- symbol_table.py
+++ symbol_table.py
@@ -9,44 +9,50 @@ from semantic_entry import SemanticEntry
 
 class SymbolTable(object):
 
-    def __init__(self, name, depth):
-        self.name = name
-        self.cur_depth = depth
+    def __init__(self, name):
+        self.name = ''
+        self.cur_depth = 0
         self.sem_records = [] #Do we need this?
         self.context_attributes_stack = []
-        self.cur_context_attributes
+        self.cur_context_attributes = None
         self.sym_table = {}
 
-    def create(self, newRecord): #subtables
+    def create(self): #subtables
         self.cur_context_attributes = ContextAttrs()
         self.cur_depth += 1
 
-    def create(self): #Root table
+    def create_root(self): #Root table
         self.cur_context_attributes = ContextAttrs()
 
     def destroy(self):
         self.cur_lexeme = ''
-
+        for lexeme in self.cur_context_attributes.context_lexemes:
+            self.cur_entry = SemanticEntry()
+            self.cur_entry = self.find(lexeme)
+            if self.cur_entry is not None:
+                self.cur_entry.back_out()
+                if self.cur_entry.semantic_record_stack is None:
+                    del self.sym_table[lexeme]
 
     def insert(self, record):
-        self.cur_context_attributes.context_lexemes.append(record.lexeme, record.size)
+        self.cur_context_attributes.context_lexemes.append(record.lexeme)
         record.offset = self.cur_depth
         self.cur_depth += record.size
         self.existing_entry = self.find(record.lexeme)
-        if existing_entry == None:
+        if self.existing_entry is None:
             self.new_entry = SemanticEntry()
             record.depth = 0
             self.new_entry.put(record)
             self.sym_table[record.lexeme] = self.new_entry
         else:
-            record.depth = existing_entry.depth
-            existing_entry.put(record)
+            record.depth = self.existing_entry.depth
+            self.existing_entry.put(record)
 
     def find(self, lexeme):
-        return sym_table[lexeme]
+         return self.sym_table.get(lexeme, None)
 
     def __repr__(self):
-        return "%s" % self.sym_table
+        return self.sym_table
 
 
 class ContextAttrs(object):
